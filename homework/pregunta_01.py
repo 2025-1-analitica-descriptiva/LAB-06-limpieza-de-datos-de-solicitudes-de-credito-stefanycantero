b617b67070d651c2f263fb40c0d2287ac9a9b31c
Stefany Cantero <stefanycanteroc@gmail.com>
2025-06-23 19:42:24 -0500

    done?

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,8 +1,6 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
-
-
 def pregunta_01():
     """
     Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
@@ -11,5 +9,32 @@ def pregunta_01():
     realizar la limpieza de los datos.
 
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
-
     """
+    import pandas as pd
+    import os
+
+    os.makedirs("files/output", exist_ok=True)
+
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep=";")
+
+    df.dropna(inplace=True)
+
+    str_cols = ["sexo", "tipo_de_emprendimiento", "idea_negocio", "barrio", "línea_credito"]
+    df[str_cols] = df[str_cols].apply(lambda col: col.str.lower().str.replace("_", " ", regex=False).str.replace("-", " ", regex=False))
+
+    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(
+        lambda x: "/".join(reversed(str(x).split("/"))) if pd.notnull(x) and len(str(x).split("/")[0]) == 4 else x
+    )
+
+    df["monto_del_credito"] = df["monto_del_credito"].str.replace("[$ ,]", "", regex=True).astype(float)
+
+    columnas_clave = [
+        "sexo", "tipo_de_emprendimiento", "idea_negocio", "barrio",
+        "estrato", "comuna_ciudadano", "fecha_de_beneficio",
+        "monto_del_credito", "línea_credito"
+    ]
+    df = df.drop_duplicates(subset=columnas_clave).dropna()
+
+    df.to_csv("files/output/solicitudes_de_credito.csv", sep=";", index=False)
+
+pregunta_01()
